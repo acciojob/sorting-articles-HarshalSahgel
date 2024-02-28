fa7d17a70a074f2f74ae66e13ac021ea085234aa
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-02-28 18:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const article = document.querySelector("div");
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function removeArticles(bandName) {
+    return bandName.replace(/\b(a |an |the )\b/gi, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => removeArticles(a) > removeArticles(b) ? 1 : -1);
+			   
+const ultag = document.createElement("ul");
+ultag.id = "band";
+
+sortedBands.forEach((item) =>{
+	const liTag = document.createElement("li");
+	liTag.textContent = item;
+	ultag.appendChild(liTag);
+});
+
+article.appendChild(ultag);
